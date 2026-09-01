17df978903cd7f77497b5ad4463269b077ff1deb
Ayushiyawale03 <ayushiayawale03@gmail.com>
2026-09-01 18:22:13 +0530

    Add Hello.java with subtraction example

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sub = a - b;
+
+        System.out.println(sub);
+    }
+}
